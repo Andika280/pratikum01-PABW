8f57cb855b882d4f9a7795c0ada0ab0693233acf
Andika280 <andikaraihan871@gmail,com>
2026-09-24 11:33:52 +0700

    update pratikum02 PABW

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
+use App\Http\Controllers\DataController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello, Andika';
+});
+
+Route::get('/user/{Andika}', function ($name) {
+    return "Halo Saya " . $name;
+});
+
+Route::get('/greet/{Andika?}', function ($name = 'Guest') {
+    return "Halo, " . $name;
+});
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/about', function () {
+    return view('about', ['name' => 'Nama Anda']);
+});
+
+Route::get('/form', [DataController::class, 'form']);
+Route::post('/proses', [DataController::class, 'proses']);
